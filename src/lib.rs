c3ff7cc3e963867d574e4e01ec3b44a3fa6b650a
Dalton Maahs <dalton.maahs@yourbind.com>
2021-02-04 02:36:20 -0600

    bump to rust edition 2018. Fix warnings. Fix Clippy all target warnings.

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,13 @@
-#![allow(dead_code)]
-
-extern crate x11;
-extern crate libc;
-extern crate image;
-use self::libc::{c_int, c_ulong};
-use std::ffi::CString;
+use libc::{c_int, c_ulong};
 use x11::xlib;
 use std::{slice, ptr};
-use self::image::{RgbImage, Rgb, Pixel};
+use image::{RgbImage, Rgb, Pixel};
 pub struct Screen {
     display: *mut xlib::Display,
     window: xlib::Window
 }
 #[derive(Debug)]
-pub struct bgr {
+pub struct Bgr {
     b: u8,
     g: u8,
     r: u8,
@@ -27,13 +21,13 @@ impl Screen {
             let screen = xlib::XDefaultScreen(display);
             let root = xlib::XRootWindow(display, screen);
             Screen{
-                display: display,
+                display,
                 window: root
             }
         }
     }
     pub fn cap_frame(&self, w: u32, h: u32, x: i32, y: i32) -> image::ImageBuffer<image::Rgb<u8>, Vec<u8>>{
-        let mut img = unsafe {
+        let img = unsafe {
         xlib::XGetImage(self.display,
                         self.window,
                         x,
@@ -46,14 +40,14 @@ impl Screen {
 
         let mut fullimg = RgbImage::new(w, h);
 
-        if(!img.is_null()){
+        if !img.is_null(){
             let image = unsafe { &mut *img };
-            let sl: &[bgr] = unsafe { slice::from_raw_parts((image).data as *const _,
+            let sl: &[Bgr] = unsafe { slice::from_raw_parts((image).data as *const _,
                 (image).width as usize * (image).height as usize)};
             let clone = fullimg.clone();
             let mut pixs = clone.enumerate_pixels();
-            for mut x in sl{
-                let (xc,yc,p) = pixs.next().unwrap();
+            for x in sl{
+                let (xc,yc,_) = pixs.next().unwrap();
                 fullimg.put_pixel(xc,yc,*Rgb::from_slice(&[x.r,x.g,x.b]));
             }
         }
