098f390e193bcef56d1590cadc4f06d1035d122d
Dalton Maahs <dalton.maahs@yourbind.com>
2021-02-04 02:52:57 -0600

    Cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
+use image::{Pixel, Rgb, RgbImage};
 use libc::{c_int, c_ulong};
+use std::{ptr, slice};
 use x11::xlib;
-use std::{slice, ptr};
-use image::{RgbImage, Rgb, Pixel};
 pub struct Screen {
     display: *mut xlib::Display,
-    window: xlib::Window
+    window: xlib::Window,
 }
 #[derive(Debug)]
 pub struct Bgr {
@@ -15,44 +15,58 @@ pub struct Bgr {
 }
 
 impl Screen {
-    pub fn new() -> Screen{
+    pub fn new() -> Screen {
         unsafe {
             let display = xlib::XOpenDisplay(ptr::null());
             let screen = xlib::XDefaultScreen(display);
             let root = xlib::XRootWindow(display, screen);
-            Screen{
+            Screen {
                 display,
-                window: root
+                window: root,
             }
         }
     }
-    pub fn cap_frame(&self, w: u32, h: u32, x: i32, y: i32) -> image::ImageBuffer<image::Rgb<u8>, Vec<u8>>{
+    pub fn cap_frame(
+        &self,
+        w: u32,
+        h: u32,
+        x: i32,
+        y: i32,
+    ) -> image::ImageBuffer<image::Rgb<u8>, Vec<u8>> {
         let img = unsafe {
-        xlib::XGetImage(self.display,
-                        self.window,
-                        x,
-                        y,
-                        w,
-                        h,
-                        !1 as c_ulong,
-                        2 as c_int)
+            xlib::XGetImage(
+                self.display,
+                self.window,
+                x,
+                y,
+                w,
+                h,
+                !1 as c_ulong,
+                2 as c_int,
+            )
         };
 
         let mut fullimg = RgbImage::new(w, h);
 
-        if !img.is_null(){
+        if !img.is_null() {
             let image = unsafe { &mut *img };
-            let sl: &[Bgr] = unsafe { slice::from_raw_parts((image).data as *const _,
-                (image).width as usize * (image).height as usize)};
+            let sl: &[Bgr] = unsafe {
+                slice::from_raw_parts(
+                    (image).data as *const _,
+                    (image).width as usize * (image).height as usize,
+                )
+            };
             let clone = fullimg.clone();
             let mut pixs = clone.enumerate_pixels();
-            for x in sl{
-                let (xc,yc,_) = pixs.next().unwrap();
-                fullimg.put_pixel(xc,yc,*Rgb::from_slice(&[x.r,x.g,x.b]));
+            for x in sl {
+                let (xc, yc, _) = pixs.next().unwrap();
+                fullimg.put_pixel(xc, yc, *Rgb::from_slice(&[x.r, x.g, x.b]));
             }
         }
 
-        unsafe {xlib::XDestroyImage(img as *mut _);}
+        unsafe {
+            xlib::XDestroyImage(img as *mut _);
+        }
         fullimg
     }
 }
